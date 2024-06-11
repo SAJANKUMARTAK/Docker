50d32278ff9419973caa4f70a50271befafba2d5
sajan <sktak100@gmail.com>
2024-06-11 13:29:01 +0000

    Docker Flask Application Python

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['input_box']
+    return f"You entered: {user_input}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
